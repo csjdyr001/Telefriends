e3d312df967c92ad9b1e5acd1ee38d5a3f992be6
csjdyr001 <>
2025-07-03 20:52:36 +0800

    上次同步：2025-07-03 12:52:36（移动端）

--- app/src/main/java/com/cfks/telefriends/utils/WatchUtils.java
+++ app/src/main/java/com/cfks/telefriends/utils/WatchUtils.java
@@ -7,26 +7,11 @@ import android.text.TextUtils;
 import com.cfks.telefriends.WatchType;
 
 public class WatchUtils {
-    private static Uri XTC_CHECK = Uri.parse("content://com.xtc.provider/BaseDataProvider/watchId/1");
-    private static Uri ZITENG_CHECK = Uri.parse("content://com.zmapp.watch/zmaeesp");
-    private static Uri QIHOO_CHECK = Uri.parse("content://com.qihoo.datacenter.WatchDataCenterProvider");
-        
-    public static int checkWatchType(Context ctx) {
-        ContentResolver cr = ctx.getContentResolver();
+    public static int getWatchType(Context ctx) {
         int result = WatchType.WATCH_OTHER;
-        Cursor cursor = cr.query(ZITENG_CHECK,new String[]{"userid1"}, null, null, null);
-        String zt = "";
-        if(cursor.moveToFirst()){
-            try {
-                zt = cursor.getString(0);
-                if (zt != null) {
-                    zt = String.valueOf(Integer.valueOf(Integer.parseInt(zt)));
-                } 
-            } catch (Exception e2) {}
-        }
-    	if(!TextUtils.isEmpty(cr.getType(XTC_CHECK))) {
+    	if(!TextUtils.isEmpty(getWatchId(ctx,WatchType.WATCH_XTC))) {
     		result = WatchType.WATCH_XTC;
-    	}else if(!TextUtils.isEmpty(zt)) {
+    	}else if(!TextUtils.isEmpty(getWatchId(ctx,WatchType.WATCH_ZITENG))) {
     		result = WatchType.WATCH_ZITENG;
     	}else{
             result = WatchType.WATCH_OTHER;
@@ -34,7 +19,32 @@ public class WatchUtils {
         return result;
     }
 
-    public String getWatchId(Context ctx,int watchType){
-        return "";
+    public static String getWatchId(Context ctx,int watchType){
+        Uri XTC = Uri.parse("content://com.xtc.provider/BaseDataProvider/watchId/1");
+        Uri ZITENG = Uri.parse("content://com.zmapp.watch/zmaeesp");
+        Uri QIHOO = Uri.parse("content://com.qihoo.datacenter.WatchDataCenterProvider");
+        
+        ContentResolver cr = ctx.getContentResolver();
+        String watchId = "";
+        switch(watchType){
+            case WatchType.WATCH_XTC:
+                watchId = cr.getType(XTC);
+                break;
+            case WatchType.WATCH_ZITENG:
+                Cursor cursor = cr.query(ZITENG,new String[]{"userid1"}, null, null, null);
+                String zt = "";
+                if(cursor.moveToFirst()){
+                    try {
+                        zt = cursor.getString(0);
+                        if (zt != null) {
+                            watchId = String.valueOf(Integer.valueOf(Integer.parseInt(zt)));
+                        } 
+                    } catch (Exception e) {
+                        watchId = "";
+                    }
+                }
+                break;
+        }
+        return watchId;
     }
 }
