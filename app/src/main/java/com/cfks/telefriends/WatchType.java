65d206a59984da67fffe2e3a23ad649c98a158e1
caofangkuai <99636121+csjdyr001@users.noreply.github.com>
2025-07-15 16:44:11 +0800

    Delete app directory

--- app/src/main/java/com/cfks/telefriends/WatchType.java
+++
@@ -1,9 +0,0 @@
-package com.cfks.telefriends;
-
-public class WatchType {
-    public static final int WATCH_XTC = 0;
-    public static final int WATCH_QIHOO = 1;
-    public static final int WATCH_MITU = 2;
-    public static final int WATCH_ZITENG = 3;
-    public static final int WATCH_OTHER = 4;
-}
